9ae64fd097066df9922eb56e3312812dd20472ee
2soul2 <animeamv682@gmail.com>
2021-10-17 23:24:43 -0300

    ahora se guardan las opciones en la base de datos

--- views/HU_crear_votacion/consultas.php
+++ views/HU_crear_votacion/consultas.php
@@ -9,28 +9,30 @@ $asunto = $_SESSION['sesion']['asunto'];
 $fechaFin = $_SESSION['sesion']['fecha'];
 $opcion = $_SESSION['sesion']['opciones'] + 1; 
 
+    $votacion = "INSERT INTO Votacion VALUES (null,'$titulo','$asunto', true ,null,'$fechaFin',0,1)";  
 
+    mysqli_query($con,$votacion);
+    
+    $idVotacionSQL = "SELECT max(id) as id from votacion";
+    $idVotacion = mysqli_query($con,$idVotacionSQL);
+    $fila = mysqli_fetch_array($idVotacion);
+    $idVotacion = $fila["id"];
 
-/* if(isset($_GET['confirmar'])){
+  if(isset($_GET['confirmar'])){
 
     for($i=0;$i<$_SESSION['sesion']['opciones'];$i++){
 
-        echo $_GET['opcion'.$i];
-
+        $op = $_GET['opcion'.$i];
+        $opciones = "INSERT INTO opcion VALUES (null,'$op',$idVotacion,0)";  
+        mysqli_query($con,$opciones);
     }
-}
- */
+  } 
 
-  $votacion = "INSERT INTO Votacion VALUES (null,'$titulo','$asunto', true ,null,'$fechaFin',0,1)";  
-
-   /*  $opcion = "INSERT INTO Opcion (id, nombre, votacion_id, cantidad_votos) VALUES (NULL,'sex',3,0)";
-    mysqli_query($con, $opcion);   */
-
-
-    mysqli_query($con, $votacion);  
-    
+    $opciones = "INSERT INTO opcion VALUES (null,'Nulo',$idVotacion,0)";  
+    mysqli_query($con,$opciones);
+   
     session_destroy();
-    require("./crearVotacion.php");
+    header("Location: ../../index.php"); 
    
 
 ?>
