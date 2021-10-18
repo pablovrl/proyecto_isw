93380c0d0c41adb6cbd571fcdfa0b2d9bfe33815
Pablo Villarroel <pablovillarroel135@gmail.com>
2021-10-18 00:09:07 -0300

    se validaron las fechas para las votaciones activas y terminadas

--- models/HU_listar_activas/cardQuery.php
+++ models/HU_listar_activas/cardQuery.php
@@ -2,7 +2,7 @@
 
 require_once("./database/db.php"); 
 
-$querySQL = "SELECT * FROM `votacion` WHERE esta_activa = '1' ORDER BY fecha_termino ASC";
+$querySQL = "SELECT * FROM `votacion` WHERE votacion.fecha_termino > NOW() ORDER BY fecha_termino ASC";
 $votacionActiva = mysqli_query($con, $querySQL);
 
 ?>
--- models/HU_listar_terminadas_query/card_query.php
+++ models/HU_listar_terminadas_query/card_query.php
@@ -1,7 +1,6 @@
-<?php 
+<?php
 include("../../database/db.php");
 
-$query = "SELECT *, max(opcion.cantidad_votos) as 'ganador' from votacion, opcion WHERE votacion.id = opcion.votacion_id and votacion.esta_activa = 0 group by votacion.id ORDER BY `ganador` DESC;
+$query = "SELECT *, max(opcion.cantidad_votos) as 'ganador' from votacion, opcion WHERE votacion.id = opcion.votacion_id and votacion.fecha_termino < NOW() group by votacion.id ORDER BY `ganador` DESC;
 ";
 $votacion = mysqli_query($con, $query);
-?>
