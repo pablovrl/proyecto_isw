fef8b0f843c4bb93cfcdfc7c64652d508e30bea3
Ignacio <ignaciogonzalez1609@gmail.com>
2021-10-24 22:54:01 -0300

    Arreglo buscador + mensaje por no encontrar resultados

--- views/HU_listar_terminadas/card.php
+++ views/HU_listar_terminadas/card.php
@@ -1,19 +1,21 @@
 <?php  include('../../models/HU_listar_terminadas_query/card_query.php');
     $votacionid = $row['votacion_id'];
-       $ganador_consulta = $con->query("SELECT * FROM opcion WHERE cantidad_votos = (SELECT MAX(cantidad_votos) FROM opcion where votacion_id = $votacionid) and votacion_id = $votacionid");
+    $ganador_consulta = $con->query("SELECT * FROM opcion WHERE cantidad_votos = (SELECT MAX(cantidad_votos) FROM opcion where votacion_id = $votacionid) and votacion_id = $votacionid");
         if(mysqli_num_rows($ganador_consulta) == 1){
             $fila = mysqli_fetch_array($ganador_consulta);
             $nombre_ganador = $fila['nombre'];
-             }
+        }else{
+            $nombre_ganador = 'Empate';
+        }
 ?>
 
 
 <div class="col-6">
-    <div class="card" style="margin-bottom: 3vh;">
+    <div class="card" style="margin-bottom: 3vh">
         <div class="card-body" style="padding: 0px !important;">
             <h5 class="card-header card-header-text" > <?php echo $row['titulo'] ?></h5>
             <div class="container">
-                <p class="card-text card-asunto"><?php echo $row['asunto'] ?></p>                                 
+                <p class="card-text card-asunto">Descripción: <?php echo $row['asunto'] ?></p>                                 
                 <div class="row">
                     <div class="col-6">
                         <p class="card-text">Cantidad de votos Total: <?php echo $row['total_votos']; ?></p>
--- views/HU_listar_terminadas/terminadas.php
+++ views/HU_listar_terminadas/terminadas.php
@@ -28,7 +28,8 @@
       </form>
       <?php if(isset($_GET['enviar'])){
         $busqueda = $_GET['busqueda'];
-        $buscadorSQL = $con->query("SELECT *, max(opcion.cantidad_votos) as 'ganador' from votacion, opcion WHERE votacion.id = opcion.votacion_id and votacion.titulo LIKE '%$busqueda%' and votacion.esta_activa = 0 group by votacion.id ORDER BY `ganador` DESC;");
+        $buscadorSQL = $con->query("SELECT *, max(opcion.cantidad_votos) as 'ganador' from votacion, opcion WHERE votacion.id = opcion.votacion_id and votacion.titulo LIKE '%$busqueda%' and votacion.fecha_termino < NOW() group by votacion.id ORDER BY `ganador` DESC;");
+        $count_results = mysqli_num_rows($buscadorSQL);
       } ?>
     <?php if(isset($buscadorSQL)){ ?>
     <form class="form-inline" action="terminadas.php">
@@ -50,9 +51,16 @@
         include("../HU_listar_terminadas/card.php");
       }
     }else{ 
-      foreach($buscadorSQL as $row){
-        include("../HU_listar_terminadas/card.php");
-      }
+      if($count_results>0){
+        foreach($buscadorSQL as $row){
+          include("../HU_listar_terminadas/card.php");
+        }
+      }else{ ?>
+        <div class="col-12" style="text-align: center;">
+          <p>No se encuentra ninguna votación con ese titulo!</p>
+        </div>
+    <?php }
+      
     }
    ?>                 
   </div>
