435080f4a81f4770d6c676b19738bb6c53722676
Pablo Villarroel <pablovillarroel135@gmail.com>
2021-10-29 12:58:36 -0300

    se agregaron las credenciales para la base de datos del servidor

--- database/db.php
+++ database/db.php
@@ -8,6 +8,11 @@ $db_user = "root";
 $db_pass = "";
 $db_name = "proyecto_isw";
 
+// $db_host = "localhost";
+// $db_user = "id17852257_root";
+// $db_pass = "AKjq_sn~Mm4/VViI";
+// $db_name = "id17852257_proyecto_isw";
+
 $con = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 
 if (mysqli_connect_errno()) {
