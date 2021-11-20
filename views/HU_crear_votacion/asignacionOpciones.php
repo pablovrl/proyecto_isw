eb6597a12db6f135dbd6a8498d916438dc5eecc8
2soul2 <animeamv682@gmail.com>
2021-11-20 15:24:51 -0300

    se mejoro la interfaz crearVotacion

--- views/HU_crear_votacion/asignacionOpciones.php
+++ views/HU_crear_votacion/asignacionOpciones.php
@@ -6,6 +6,7 @@
     
    $numeroName = 0; 
    $numeroOpcion = 1;
+   const boton = false;
 
    // Almacenar datos en la sesión
    if(isset($_GET['siguiente'])){
@@ -25,16 +26,16 @@
 <div class="container col-12 d-flex align-items-center justify-content-center"
       style="height: 94vh">  
         
-        <div class="shadow-lg p-3 mb-5 col-sm-10 col-lg-7">
+        <div class="shadow-lg p-5 mb-4 bg-white rounded-lg col-sm-12 col-lg-6">
         
-            <form method="GET" action="<?="consultas.php"?>"> 
-              <div class="col-12 mt-1 pl-2 mb-3">
+            <form id="form" method="GET" action=""> 
+              <div class="col-12 pl-2 mb-5">
                 <h1 class="h1">Asignación de opciones</h1>
               </div>
 
                 <?php 
                   for($i=0;$i<$option;$i++){
-                    echo '<div class="col-12 pl-2"><input class="form-control rounded-lg" type="text" name="opcion'.$numeroName++.'" placeholder="opcion'.$numeroOpcion++.'" required></div><br>';
+                    echo '<div class="col-12 pl-2 mb-3"><input class="form-control rounded-lg"  style="background-color: #ffff; font-size: 20px; height: 60px;"  type="text" name="opcion'.$numeroName++.'" placeholder="opción'.$numeroOpcion++.'" required></div><br>';
                     
                   }
 
@@ -47,16 +48,26 @@
                 ?>
               
 
-                <div class="col-12">
-                    <!-- <a name="atras" class="btn btn-success botonAtras" href=>Atras</a> -->
-                    <button  name="confirmar" class="btn btn-success float-right">Confirmar</button>
+                <div class="col-12 pl-2">
+                    <button name="atras" class="btn btn-success float-left" onclick="actionForm(this.form.id, 'crearVotacion.php'); return false;" >Atras</button>
+                    <button  name="confirmar" class="btn btn-success float-right" onclick="actionForm(this.form.id, 'consultas.php'); return false;"  >Confirmar</button>
+
+                    <script>
+                      function actionForm(form, act)
+                      {
+                          document.getElementById(form).action=act;
+                          document.getElementById(form).submit();
+                      }
+                    </script>
 
                 </div>
+
                   <input type="hidden" name="titulo" value="<?=$titulo?>"/>
                   <input type="hidden" name="asunto" value="<?=$asunto?>"/>
                   <input type="hidden" name="fecha" value="<?=$fecha?>"/>
                   
                   <input type="hidden" name="opciones" value="<?=$option?>" />
+
 
             </form>
           
--- views/HU_crear_votacion/crearVotacion.php
+++ views/HU_crear_votacion/crearVotacion.php
@@ -1,67 +1,74 @@
+
+
 <?php require("../base/header.php") ?>
 
     <?php 
       $titulo="";
       $asunto="";
-      $fecha="";
-
+   
       
       if(isset($_GET['fecha'])){
         
         $titulo = $_GET['titulo'];
         $asunto = $_GET['asunto'];
-        $fecha = $_GET['fecha'];
+
+        
       }
       
     
     ?>
     
-    
   <div class="container col-12 d-flex align-items-center justify-content-center"
         style="height: 94vh";> 
-    <div class="shadow-lg p-3 mb-5 bg-white rounded-lg col-sm-12 col-lg-7">
+    <div class="shadow-lg p-5 mb-4 bg-white rounded-lg col-sm-12 col-lg-6">
       <form method="GET" action="asignacionOpciones.php">
-              <div class="col-12 mt-1 pl-3 mb-3">
-                  <h1 class="text-left h1">Creación Formulario</h1>
+              <div class="col-12 mt-0 pl-3 mb-4">
+                  <h1 class="text-left h1">Creación de votación</h1>
               </div>
-              <div class="col-12 ml-1 mb-3"> 
+              <div class="col-12 ml-1 mb-4"> 
+                  <lable class="text-left h4" for="título">Título</lable>
                   <input class="rounded-lg h-60 bg form-control" 
-                  style="background-color: #ffff; font-size: 20px; height: 60px;"
-                  type="text" name="titulo"  value="<?=$titulo?>" onpaste="return false" placeholder="Introduzca el titulo de la votación..." required/> 
+                  style="background-color: #ffff; font-size: 20px; height: 60px; margin-top: 5px"
+                  type="text" name="titulo" maxlength="200" value="<?=$titulo?>" placeholder="Ejemplo: Elección de presidente de la junta de vecinos" required/> 
               </div>
-              <div class="col-12 ml-1 mb-3"> 
-                  <textarea rows="2" class="pt-2 form-control rounded-lg" 
-                  style="background-color: #ffff; resize: none; font-size: 20px; height: 120px; "
-                  type="textarea" name="asunto" placeholder="Introduzca el asunto de la votación..." required><?=$asunto?></textarea> 
+              <div class="col-12 ml-1 mb-5"> 
+                  <lable class="text-left h4" for="asunto">Asunto</lable>
+                  <textarea rows="2" class="pt-2 form-control rounded-lg"  id="textarea"
+                  style="background-color: #ffff; resize: none; font-size: 20px; height: 120px; margin-top: 5px "
+                  maxlength="255" type="textarea" name="asunto" placeholder="Ejemplo: Votación para escoger presidente de la junta de vecinos" required><?=$asunto?></textarea> 
               </div>
-              <div class="col-12 ml-1 mb-3"
+              <div class="col-12 ml-1 mb-4"
                     style="font-size: 26px;"> 
-                  <label for="opciones">Cantidad de opciones: </label>
+                  <label for="opciones">Número de opciones de la votación: </label>
 
-                  <select class="form-select form-select-lg ml-3 mb-3" name="opcion" id="opciones">
-                      <option value=2>2</option>
-                      <option value=3>3</option>
-                      <option value=4>4</option>
+                  <select class="form-select form-select-lg ml-3 mb-4" name="opcion" id="opciones">
+                      <option  value=2>2</option>
+                      <option  value=3>3</option>
+                      <option  value=4>4</option>
                   </select>
+                  
               </div>
-              <div class="col-12 ml-1 mb-3" style="font-size:26px;">
-                  <label for="fecha">Fecha de termino:   </label>
+              <div class="col-12 ml-1 mb-4" style="font-size:26px;">
+                  <label for="fecha">Fecha de término de la votación:   </label>
 
                   <input class="ml-3" type="datetime-local" id="fecha" name="fecha"
                   style="font-size:20px;"
-                  name="meeting-time" min="" max="" value=<?=$fecha?> required>
+                  name="meeting-time" min="" max="" required>
 
               </div>
-              <div class="col-12">
+              <div class="col-12 ">
                   <button type="submit" style="width: 100px; border:none;"
-                  class="btn btn-success float-right" name="siguiente" >Siguiente</button>
+                  class="btn btn-success float-right " name="siguiente" >Siguiente</button>
               </div>
+              
               
               <script src="fechaMinima.js"></script>
                 
+              
           </div>
       </form>      
       
   </div>
+ 
 
   <?php require("../base/footer.php") ?>
