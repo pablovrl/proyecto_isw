0efb5b07de0b5a588b3b03c0b7bff1f20cf80594
Pablo Villarroel <pablovillarroel135@gmail.com>
2021-12-22 01:11:27 -0300

    se agregaron validaciones a los comentarios

--- views/HU_comentar/comentarios.php
+++ views/HU_comentar/comentarios.php
@@ -51,7 +51,7 @@ require("../../models/HU_comentar/getComentarios.php")
         <form action="../../models/HU_comentar/addComentario.php" method="POST">
           <input type="hidden" name="id" value=<?= $id ?>>
           <div class="form-group">
-            <input class="form-control" type="text" name="comentario" placeholder="Escribe aquí tu comentario">
+            <input required pattern=".*\S+.*" class="form-control" type="text" name="comentario" placeholder="Escribe aquí tu comentario">
           </div>
           <div class="form-group">
             <button class="btn btn-primary float-right" type="submit" name="submit">Comentar</button>
